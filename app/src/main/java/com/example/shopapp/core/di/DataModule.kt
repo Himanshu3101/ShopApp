73643b3b9bbc2b5c84b922e5322eaacb2e928680
Himanshu <himanshushrivastwa101@gmail.com>
2025-05-18 11:28:14 +0530

    Structured the Project.

--- app/src/main/java/com/example/shopapp/core/di/DataModule.kt
+++ app/src/main/java/com/example/shopapp/core/di/DataModule.kt
@@ -1,11 +1,8 @@
 package com.example.shopapp.core.di
 
-import com.example.shopapp.core.network.ShopApi
 import com.example.shopapp.core.network.secrets.AuthInterceptor
 import com.example.shopapp.core.network.secrets.KeysProvider.AppKeysProviderImpl
 import com.example.shopapp.core.network.secrets.KeysProvider.KeysProvider
-import com.example.shopapp.features.dashboard.data.repository.BannerRepositoryImpl
-import com.example.shopapp.features.dashboard.domain.remote.repositoy.BannerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
