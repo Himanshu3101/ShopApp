cc3d51bfd862681468309cd75af31153b530b14f
Himanshu <himanshushrivastwa101@gmail.com>
2025-05-18 14:16:01 +0530

    Banner API Done till viewModel.

---
+++ app/src/main/java/com/example/shopapp/features/dashboard/di/GetBannerModule.kt
@@ -0,0 +1,20 @@
+package com.example.shopapp.features.dashboard.di
+
+import com.example.shopapp.features.dashboard.domain.remote.repositoy.BannerRepository
+import com.example.shopapp.features.dashboard.domain.useCases.GetBanner_UC
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GetBannerModule {
+
+    @Provides
+    @Singleton
+    fun provideGetBannerUseCases(bannerRepository: BannerRepository): GetBanner_UC {
+        return GetBanner_UC(bannerRepository)
+    }
+}
