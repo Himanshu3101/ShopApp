6dc4a7ebecad130be3235f3a282fca1e51950bf5
Himanshu <himanshushrivastwa101@gmail.com>
2025-05-23 19:57:44 +0530

    Recommended items are dynamically showed and working on dynamically height and width.

--- app/src/main/java/com/example/shopapp/features/dashboard/presentation/screen/component/GridWith_Images_Details.kt
+++ app/src/main/java/com/example/shopapp/features/dashboard/presentation/screen/component/GridWith_Images_Details.kt
@@ -1,6 +1,5 @@
 package com.example.shopapp.features.dashboard.presentation.screen.component
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,11 +11,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,13 +26,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.shopapp.R
-import com.example.shopapp.core.util.Constants.deviceSize
-import com.example.shopapp.core.util.Constants.log
 import com.example.shopapp.features.dashboard.presentation.screen.state.ItemData
 import com.example.shopapp.ui.theme.Dimens
 
