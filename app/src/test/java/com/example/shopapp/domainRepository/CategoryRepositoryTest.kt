1b0f47957cff4c9d4d08622e0222220699b1ba64
Himanshu <himanshushrivastwa101@gmail.com>
2025-05-25 18:40:44 +0530

    Done test cases for features-dashboard-domain-repository

--- app/src/test/java/com/example/shopapp/domainRepository/CategoryRepositoryTest.kt
+++ app/src/test/java/com/example/shopapp/domainRepository/CategoryRepositoryTest.kt
@@ -1,7 +1,71 @@
 package com.example.shopapp.domainRepository
 
+import com.example.shopapp.core.network.ShopApi
+import com.example.shopapp.features.dashboard.data.remote.dto.CategoryResponse
+import com.example.shopapp.features.dashboard.data.repository.CategoryRepositoryImpl
+import com.example.shopapp.features.dashboard.domain.remote.model.Category
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
 class CategoryRepositoryTest {
 
+    private lateinit var mockShopApi: ShopApi
 
+    private lateinit var categoryRepository: CategoryRepositoryImpl
+
+    @Before
+    fun setUp(){
+        mockShopApi = mock()
+        categoryRepository = CategoryRepositoryImpl(mockShopApi)
+    }
+
+    @Test
+    fun `getCategory return a list of category on successfull API response`() = runTest{
+
+        val expectedCategories = listOf(
+            Category(objectId = "banner_id_1", createdAt = "Today", updatedAt = "TodY", title = "url_1", Pid = 1),
+            Category(objectId = "banner_id_2", createdAt = "yESTERday", updatedAt = "yESTerday", title = "url_2", Pid = 2)
+        )
+
+        val apiResponse = CategoryResponse(expectedCategories)
+
+        whenever(mockShopApi.getCategory()).thenReturn(apiResponse)
+
+        val actualCategories = categoryRepository.getCategory()
+
+        assert(expectedCategories.size == actualCategories.size)
+        assert(expectedCategories == actualCategories)
+    }
+
+    @Test
+    fun `getCategory return an EMPTY list when API response is empty`() = runTest{
+        val emptyApiResponse = CategoryResponse(results = emptyList())
+
+        whenever(mockShopApi.getCategory()).thenReturn(emptyApiResponse)
+
+        val actualCategory = categoryRepository.getCategory()
+
+        assertTrue(actualCategory.isEmpty())
+        assertEquals(0, actualCategory.size)
+    }
+
+    @Test
+    fun `getCategory throws exception when API call fails`() = runTest {
+        val expectedErrorMessage = "Network error occurred"
+
+        whenever(mockShopApi.getCategory()).thenAnswer { throw Exception (expectedErrorMessage) }
+
+        val exception = assertFailsWith < Exception>(message = "Network error occurred") {
+            categoryRepository.getCategory() // This block is expected to throw the exception
+        }
+
+        assertEquals(expectedErrorMessage, exception.message)
+    }
 
 }
---
+++ app/src/test/java/com/example/shopapp/domainRepository/ItemRepositoryTest.kt
@@ -0,0 +1,81 @@
+package com.example.shopapp.domainRepository
+
+import com.example.shopapp.core.network.ShopApi
+import com.example.shopapp.features.dashboard.data.remote.dto.ItemsResponse
+import com.example.shopapp.features.dashboard.data.repository.ItemRepositoryImpl
+import com.example.shopapp.features.dashboard.domain.remote.model.Items
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class ItemRepositoryTest {
+
+    private lateinit var mockShopApi: ShopApi
+    private lateinit var itemRepository: ItemRepositoryImpl
+
+    @Before
+    fun setUp() {
+        mockShopApi = mock()
+        itemRepository = ItemRepositoryImpl(mockShopApi)
+    }
+
+    @Test
+    fun `getItems return a list of items on successfull API response`() = runTest {
+
+        val expectedItems = listOf(
+            Items(categoryId = 0, createdAt = "Today", description = "ABC", objectId = "banner_id_1", updatedAt = "TodY",
+                price = 1, rating = 2.0, showRecommended = true, title = "url_1",
+                picUrl = listOf(
+                    "https://res.cloudinary.com/dkikc5ywq/image/upload/v1746098536/1_1_db56nv.png",
+                    "https://res.cloudinary.com/dkikc5ywq/image/upload/v1746098532/1_4_vdstgc.jpg"
+                )
+            ),
+            Items(categoryId = 1, createdAt = "yESTERday", description = "DEF", objectId = "banner_id_2", updatedAt = "yESTerday",
+                title = "url_2", price = 1, rating = 2.0, showRecommended = true,
+                picUrl = listOf(
+                    "https://res.cloudinary.com/dkikc5ywq/image/upload/v1746098536/1_1_db56nv.png",
+                    "https://res.cloudinary.com/dkikc5ywq/image/upload/v1746098532/1_4_vdstgc.jpg"
+                )
+            )
+        )
+
+        val apiResponse = ItemsResponse(expectedItems)
+
+        whenever(mockShopApi.getItems()).thenReturn(apiResponse)
+
+        val actualItems = itemRepository.getItems()
+
+        assert(expectedItems.size == actualItems.size)
+        assert(expectedItems == actualItems)
+    }
+
+    @Test
+    fun `getItems return an EMPTY list when API response is empty`() = runTest{
+        val emptyApiResponse = ItemsResponse(results = emptyList())
+
+        whenever(mockShopApi.getItems()).thenReturn(emptyApiResponse)
+
+        val actualItems = itemRepository.getItems()
+
+        assertTrue(actualItems.isEmpty())
+        assertEquals(0, actualItems.size)
+    }
+
+    @Test
+    fun `getItems throws exception when API call fails`() = runTest {
+        val expectedErrorMessage = "Network error occurred"
+
+        whenever(mockShopApi.getItems()).thenAnswer { throw Exception (expectedErrorMessage) }
+
+        val exception = assertFailsWith < Exception>(message = "Network error occurred") {
+            itemRepository.getItems() // This block is expected to throw the exception
+        }
+
+        assertEquals(expectedErrorMessage, exception.message)
+    }
+}
