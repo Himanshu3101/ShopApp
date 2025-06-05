a2af481e3fd2c7d5419d405e630b12d3b35aaa9a
Himanshu <himanshushrivastwa101@gmail.com>
2025-06-05 16:08:39 +0530

    Done - Product type list Show and structure done

--- app/src/main/java/com/example/shopapp/features/dashboard/domain/UseCases_Module/GetBannerModule.kt
+++ app/src/main/java/com/example/shopapp/features/dashboard/domain/UseCases_Module/GetBannerModule.kt
@@ -1,4 +1,4 @@
-package com.example.shopapp.features.dashboard.domain.Module
+package com.example.shopapp.features.dashboard.domain.UseCases_Module
 
 import com.example.shopapp.features.dashboard.domain.remote.repositoy.BannerRepository
 import com.example.shopapp.features.dashboard.domain.useCases.GetBanner_UC
